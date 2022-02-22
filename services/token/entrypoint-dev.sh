056c2932c35955048f5dc170026c506ba7d36f31
jhordyr huaman <jhordyrx@gmail.com>
2022-02-22 02:57:11 -0500

    chnge dokce

--- services/token/entrypoint-dev.sh
+++ services/token/entrypoint-dev.sh
@@ -6,6 +6,6 @@ while ! nc -z token-db 3306; do
   sleep 0.1
 done
 
-echo "Mysql iniciado"
+echo "Mysql iniciado:w"
 
-gunicorn -b 0.0.0.0:5000 manage:app
+python manage.py run -h 0.0.0.0
--- services/token/entrypoint.sh
+++ services/token/entrypoint.sh
@@ -6,6 +6,6 @@ while ! nc -z token-db 3306; do
   sleep 0.1
 done
 
-echo "Mysql iniciado:w"
+echo "Mysql iniciado"
 
-python manage.py run -h 0.0.0.0
+gunicorn -b 0.0.0.0:5000 manage:app
