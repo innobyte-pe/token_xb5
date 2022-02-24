8c22e60ed43c1888e28bc51477b066020180605e
jhordyr huaman <jhordyrx@gmail.com>
2022-02-23 22:26:10 -0500

    dddd

--- services/token/project/__init__.py
+++ services/token/project/__init__.py
@@ -6,12 +6,12 @@ import os
 from flask import Flask  # nuevo
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
-
+from flask_cors import CORS  # nuevo
 
 # instanciando la db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
-
+cors = CORS()  # nuevo
 # new
 def create_app(script_info=None):
 
@@ -25,7 +25,7 @@ def create_app(script_info=None):
     # preparando la extensión
     db.init_app(app)
     toolbar.init_app(app) # nuevo
-
+    cors.init_app(app)
     # registrar blueprints
     from project.api.index import users_blueprint
 
